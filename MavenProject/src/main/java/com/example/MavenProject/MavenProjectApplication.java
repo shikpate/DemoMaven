f687c2eba507f34c824988b0ddf34839dabdbc56
shikpate <44962270+shikpate@users.noreply.github.com>
2018-11-13 14:54:57 +0530

    Update MavenProjectApplication.java

--- MavenProject/src/main/java/com/example/MavenProject/MavenProjectApplication.java
+++ MavenProject/src/main/java/com/example/MavenProject/MavenProjectApplication.java
@@ -8,5 +8,6 @@ public class MavenProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenProjectApplication.class, args);
+		System.out.println("Hello World");
 	}
 }
