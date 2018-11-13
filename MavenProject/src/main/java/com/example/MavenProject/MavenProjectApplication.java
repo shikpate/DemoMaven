fdcbb2cf30cfa7f979775f1978629688d21021ec
shikhar <shikhar.patel@capgemini.com>
2018-11-13 14:46:52 +0530

    file added

---
+++ MavenProject/src/main/java/com/example/MavenProject/MavenProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.MavenProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenProjectApplication.class, args);
+	}
+}
